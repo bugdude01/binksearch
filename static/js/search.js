99fb2ee336d89e7c48802db69cd20a4f5de8b5bf
bugdude01 <fafrhodes@yahoo.co.uk>
2018-04-24 18:02:53 +0100

    add static and requirements

---
+++ static/js/search.js
@@ -0,0 +1,9 @@
+(function() {
+var cx = '009259304065666358809:n8yu030d1lu';
+var gcse = document.createElement('script');
+gcse.type = 'text/javascript';
+gcse.async = true;
+gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(gcse, s);
+})();
